a59f81916a25bc119df6deaeb748bb4a5e4b42f3
Lilya Benladjreb <lilyabenladjreb@Lilyas-MacBook-Air.local>
2022-03-08 19:12:21 -0500

    first commit

---
+++ fichiersFournisTP2H22/ListeVideException.java
@@ -0,0 +1,15 @@
+
+/**
+ * <p>Classe d'exception implicite utilisee pour signaler qu'une operation ne 
+ * peut pas etre effectuee sur une liste, car celle-ci est vide.</p>
+ * @author Melanie Lord
+ * @version Hiver 2022
+ */
+public class ListeVideException extends RuntimeException {
+   
+   /**
+    * <p>Construit une ListeVideException, sans message.</p>
+    */
+   public ListeVideException () {}
+   
+}
